a522b83e8b56d7608facc0476f37c96e5bb2e350
Josep Servat <jservat@outlook.es>
2022-05-17 01:32:35 +0200

    Add josep authority

--- src/App.js
+++ src/App.js
@@ -53,20 +53,17 @@ export default function App() {
     <div className="mainContainer">
     <div className="dataContainer">
       <div className="header">
-      👋 Hey there!
+        👋 Hey there!
       </div>
 
       <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Josep and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
       </div>
 
       <button className="waveButton" onClick={null}>
         Wave at Me
       </button>
 
-      {/*
-      * If there is no currentAccount render this button
-      */}
       {!currentAccount && (
         <button className="waveButton" onClick={connectWallet}>
           Connect Wallet
