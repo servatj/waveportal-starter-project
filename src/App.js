915b113219539d34dba95fb078bf5b0de4b3f42d
Josep Servat <jservat@outlook.es>
2022-05-17 01:28:53 +0200

    Add src

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 export default function App() {
 
-  const wave = () => {
-    
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log('Make sure you have metamask');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum)
+      }
+      const accounts = await ethereum.requests({ method: 'eth_accounts' })
+      if (accounts.length !== 0) {
+        const [account,] = accounts;
+        console.log('Found and authorized account', account);
+        setCurrentAccount(account);
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch(error){
+      console.log(error);
+    }
   }
-  
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
-
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
+    <div className="dataContainer">
+      <div className="header">
+      👋 Hey there!
       </div>
+
+      <div className="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+      </div>
+
+      <button className="waveButton" onClick={null}>
+        Wave at Me
+      </button>
+
+      {/*
+      * If there is no currentAccount render this button
+      */}
+      {!currentAccount && (
+        <button className="waveButton" onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      )}
     </div>
+  </div>
   );
 }
